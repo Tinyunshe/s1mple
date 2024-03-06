9f835e3bf5a92f3e3d1ade28c3c0a55bab723f04
tinyunshe <441811842@qq.com>
2024-03-07 00:21:35 +0800

    auth first

--- auth/auth.go
+++ auth/auth.go
@@ -1,2 +1,39 @@
 package auth
 
+import (
+	"crypto/sha256"
+	"crypto/subtle"
+	"net/http"
+)
+
+var (
+	adminUsername     = "admin"
+	adminUserPassword = "1qazXSW@"
+)
+
+func AuthFailed(w http.ResponseWriter, message string) {
+	w.Header().Set("WWW-Authenticate", `Basic realm="My REALM"`)
+	w.WriteHeader(401)
+	w.Write([]byte(message))
+}
+
+func BasicAuth(nextFunc http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		username, password, ok := r.BasicAuth()
+		if !ok {
+			usernameHash := sha256.Sum256([]byte(username))
+			passwordHash := sha256.Sum256([]byte(password))
+			expectedUsernameHash := sha256.Sum256([]byte(adminUsername))
+			expectedPasswordHash := sha256.Sum256([]byte(adminUserPassword))
+
+			usernameMatch := subtle.ConstantTimeCompare(usernameHash[:], expectedUsernameHash[:]) == 1
+			passwordMatch := subtle.ConstantTimeCompare(passwordHash[:], expectedPasswordHash[:]) == 1
+
+			if usernameMatch && passwordMatch {
+				nextFunc(w, r)
+				return
+			}
+			AuthFailed(w, "Server need basic auth")
+		}
+	}
+}
--- test_client/main.go
+++ test_client/main.go
@@ -2,20 +2,28 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"net/http"
-	"strings"
+	"s1mple/auth"
 )
 
+// func main() {
+// 	postData := `{"id":"3","jira":"http://xxx","assignee":"stwu@alauda.io","subject":"docker异常","content":"docker异常怀疑机器导致","comments":"1、排查与机器有关"}`
+// 	contentType := "application/json"
+// 	datar := strings.NewReader(postData)
+// 	resp, _ := http.Post("http://43.140.214.63:8081/release_confluence_document", contentType, datar)
+
+// 	rb, _ := io.ReadAll(resp.Body)
+
+// 	defer resp.Body.Close()
+// 	fmt.Printf("result:\n%v\n", string(rb))
+
+// }
+
+func test(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "ok")
+}
+
 func main() {
-	postData := `{"id":"3","jira":"http://xxx","assignee":"stwu@alauda.io","subject":"docker异常","content":"docker异常怀疑机器导致","comments":"1、排查与机器有关"}`
-	contentType := "application/json"
-	datar := strings.NewReader(postData)
-	resp, _ := http.Post("http://43.140.214.63:8081/release_confluence_document", contentType, datar)
-
-	rb, _ := io.ReadAll(resp.Body)
-
-	defer resp.Body.Close()
-	fmt.Printf("result:\n%v\n", string(rb))
-
+	http.HandleFunc("/", auth.BasicAuth(test))
+	http.ListenAndServe(":30002", nil)
 }
