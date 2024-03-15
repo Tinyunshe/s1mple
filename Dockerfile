c2b5aca2a4904b0c35773bec0f9aa801a7a3b942
tinyunshe <441811842@qq.com>
2024-03-15 17:44:34 +0800

    fix build file

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,5 @@ FROM alpine:latest
 USER root
 COPY s1mple /usr/local/bin
 RUN mkdir /opt/s1mple && chmod +x /usr/local/bin/s1mple
+COPY doc_go_template.txt /opt/s1mple
 ENTRYPOINT ["s1mple"]
--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 image=$1
 img_dir="/opt/s1mple"
 docker container rm -f s1mple
-docker run -itd --name s1mple -p 8081:8080 -v ./s1mple_config_prod.yaml:$img_dir/s1mple_config.yaml -v ./doc_go_template.txt:$img_dir/doc_go_template.txt $image --config $img_dir/s1mple_config.yaml
+docker run -itd --name s1mple -p 8081:8080 -v ./s1mple_config_prod.yaml:$img_dir/s1mple_config.yaml $image --config $img_dir/s1mple_config.yaml
